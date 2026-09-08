be56e88dc6bd42a8d25ef8b19db9ca37d888b802
Luxcih <luxcih.me@gmail.com>
2026-09-08 16:40:37 +0800

    test(display): add Display tests

--- src/Display.zig
+++ src/Display.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const Display = union(enum) {
     unix: Unix,
     tcp: Tcp,
@@ -12,3 +14,43 @@ pub const Display = union(enum) {
         number: u16,
     };
 };
+
+test "unix display number" {
+    const display: Display = .{
+        .unix = .{ .number = 0 },
+    };
+
+    try std.testing.expectEqual(
+        @as(u16, 0),
+        display.unix.number,
+    );
+}
+
+test "unix display path" {
+    const display: Display = .{
+        .unix = .{ .path = "/tmp/.X11-unix/X0" },
+    };
+
+    try std.testing.expectEqualStrings(
+        "/tmp/.X11-unix/X0",
+        display.unix.path,
+    );
+}
+
+test "tcp display" {
+    const display: Display = .{
+        .tcp = .{
+            .host = "localhost",
+            .number = 0,
+        },
+    };
+
+    try std.testing.expectEqualStrings(
+        "localhost",
+        display.tcp.host,
+    );
+    try std.testing.expectEqual(
+        @as(u16, 0),
+        display.tcp.number,
+    );
+}
